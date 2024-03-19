b5bc47283dc1742aabec62eea804f53d4b99adcc
Dovbii_IKM722a <daniel@>
2024-03-19 22:32:49 +0200

    Добавьте файлы проекта.

---
+++ task1_v5_Dovbii/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ Напишіть програму, яка знаходить: суму всіх елементів у заданому
+масиві.
+*/
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 5, 3, 7, 9, 1 };
+        int sum = SumOfArrayElements(array);
+        Console.WriteLine($"Sum of all array elements: {sum}");
+    }
+
+    static int SumOfArrayElements(int[] arr)
+    {
+        int sum = 0;
+        foreach (int num in arr)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+}
